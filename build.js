902289468912b23746b7dc8256b66a11203f50c5
Tim Gallant <me@timgallant.us>
2016-06-15 00:17:08 -0700

    refactoring. switching from scss to postcss

--- build.js
+++ build.js
@@ -2,43 +2,50 @@
 
 'use strict';
 
-let metalsmith = require('metalsmith'),
-		markdown = require('metalsmith-markdown'),
-		layouts = require('metalsmith-layouts'),
-		permalinks = require('metalsmith-permalinks'),
-		assets = require('metalsmith-assets'),
-		scss = require('./plugins/scss');
+const metalsmith = require('metalsmith');
+const markdown = require('metalsmith-markdown');
+const layouts = require('metalsmith-layouts');
+const permalinks = require('metalsmith-permalinks');
+const assets = require('metalsmith-assets');
+const postcss = require('metalsmith-postcss');
 
 const meta = {
-	title: "embedd.io | the best option for embedding reddit and hacker news comments",
-	description: "Embedd allows you to show off your Reddit and HackerNews comments " +
-		"right on your blog or product page. The painless setup makes it easy " +
-		"to get started right away",
-	keywords: "reddit hackernews hacker news hn embedd js javascript blog wordpress landing page"
+	title : "embedd.io | the best option for embedding reddit and hacker news comments",
+	description : "Embedd allows you to show off your Reddit and HackerNews comments right on your blog or product page. The painless setup makes it easy to get started right away",
+	keywords : "reddit hackernews hacker news hn embedd js javascript blog wordpress landing page"
 };
 
 function build() {
 	metalsmith(__dirname)
 		.metadata(meta)
 		.use(markdown())
-		.use(layouts({ engine: 'handlebars', partials: 'partials' }))
+		.use(layouts({ engine : 'handlebars', partials : 'partials' }))
 		.use(permalinks('posts/:title'))
 		.use(assets({
-			source: './js',
+			source: './assets',
 			destination: './'
 		}))
+    .use(postcss({
+      plugins : {
+        'postcss-cssnext' : {},
+        'postcss-simple-vars' : {},
+        'css-mqpacker' : {},
+        'postcss-neat' : {},
+        'postcss-normalize' : {},
+        'postcss-csso' : {}
+      }
+    }))
 		.build(err => {
 			if(err) { throw err; }
-			scss();
 		});
 }
 
 build();
 
 if(process.argv.indexOf('-w') > -1) {
-	let http = require('http'),
-			serveStatic = require('serve-static'),
-			finalhandler = require('finalhandler');
+	const http = require('http');
+	const serveStatic = require('serve-static');
+	const finalhandler = require('finalhandler');
 
 	let serve = serveStatic("./build");
 
@@ -46,6 +53,6 @@ if(process.argv.indexOf('-w') > -1) {
 		let done = finalhandler(req, res);
 		serve(req, res, done);
 	});
-
 	server.listen(8000);
+  console.log('server started on port 8000');
 }
