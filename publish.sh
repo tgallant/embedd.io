56c83f62949c3576a059577355f1702bee1aed65
Tim Gallant <me@timgallant.us>
2016-09-24 17:27:53 -0700

    adds cache clear to publish.sh

--- publish.sh
+++ publish.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
+
+# deploy to AWS
 LOCATION=embedd.io
 DIR=build/
 aws s3 sync $DIR s3://$LOCATION --region us-west-2
+
+# Clear Cloudflare Cache
+curl -X DELETE "https://api.cloudflare.com/client/v4/zones/${CF_ZONE}/purge_cache" \
+     -H "X-Auth-Email: ${CF_EMAIL}" \
+     -H "X-Auth-Key: ${CF_KEY}" \
+     -H "Content-Type: application/json" \
+     --data '{"purge_everything":true}'
